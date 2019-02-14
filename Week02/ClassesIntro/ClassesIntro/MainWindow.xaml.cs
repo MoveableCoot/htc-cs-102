0cf4be5407a4d15ba380b6d8c34dcca98224c9b7
MoveableCoot <rsaghy04@gmail.com>
2019-02-13 19:16:29 -0500

    Made a doge

--- Week02/ClassesIntro/ClassesIntro/MainWindow.xaml.cs
+++ Week02/ClassesIntro/ClassesIntro/MainWindow.xaml.cs
@@ -27,12 +27,15 @@ namespace ClassesIntro
 
         private void Bark_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Dog Doge = new ClassesIntro.Dog();
+            Doge.Bark();
         }
 
         private void SayName_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Dog Doge = new ClassesIntro.Dog();
+            Doge.Name = "Dog";
+            Doge.SayName();
         }
     }
 }
