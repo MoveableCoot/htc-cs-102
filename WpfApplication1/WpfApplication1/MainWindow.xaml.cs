d51bb6d3f83c0d7efad2b9fa6be14055b2e11deb
MoveableCoot <rsaghy04@gmail.com>
2019-04-03 19:33:52 -0400

    Worked on abstract classes

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Rectangle myRect = new Rectangle();
+            myRect.Height = 100;
+            myRect.Width = 100;
+            myRect.Fill = Brushes.Red;
+            myRect.Stroke = Brushes.Black;
+            myRect.StrokeThickness = 2;
+
+            Canvas.SetLeft(myRect, 100);
+            Canvas.SetTop(myRect, 100);
+            myCanvas.Children.Add(myRect);
+
+
+            Diamond myDiam = new Diamond();
+            myDiam.Height = 50;
+            myDiam.Width = 50;
+            myDiam.Fill = Brushes.Blue;
+            myDiam.Stroke = Brushes.Orange;
+            myDiam.StrokeThickness = 7;
+
+            Canvas.SetLeft(myDiam, 300);
+            Canvas.SetTop(myDiam, 300);
+            myCanvas.Children.Add(myDiam);
+
+            Triangle myTri = new Triangle();
+            myTri.Height = 150;
+            myTri.Width = 150;
+            myTri.Fill = Brushes.Black;
+            myTri.Stroke = Brushes.Blue;
+            myTri.StrokeThickness = 7;
+
+            Canvas.SetLeft(myDiam, 200);
+            Canvas.SetTop(myDiam, 200);
+            myCanvas.Children.Add(myTri);
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication1
+{
+    public class Triangle : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                PathFigure myPathFigure = new PathFigure();
+                myPathFigure.StartPoint = new Point(Width / 2, 0);
+
+                LineSegment segment1 = new LineSegment();
+                segment1.Point = new Point(Width, Height / 2);
+                LineSegment segment2 = new LineSegment();
+                segment2.Point = new Point(Width / 2, Height);
+               
+
+                PathSegmentCollection myPathSegmentCollection = new PathSegmentCollection();
+                myPathSegmentCollection.Add(segment1);
+                myPathSegmentCollection.Add(segment2);
+                
+
+                myPathFigure.Segments = myPathSegmentCollection;
+
+                PathFigureCollection myPathFigureCollection = new PathFigureCollection();
+                myPathFigureCollection.Add(myPathFigure);
+
+                PathGeometry myPathGeometry = new PathGeometry();
+                myPathGeometry.Figures = myPathFigureCollection;
+
+                return myPathGeometry;
+            }
+        }
+    }
+}
